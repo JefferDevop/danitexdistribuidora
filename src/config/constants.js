ab629200b7e72df7b4189d3c0f051558d6ff7fb4
JefferDevop <somadesarrollo@gmail.com>
2025-04-21 13:34:43 -0500

    change hosting conexion.bar

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const BASE_API =
-  "https://danitexdistribuidora.catalogointeractivo.com.co";
+  "https://danitexdistribuidora.conexion.bar";
 
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
