2617738e3cb55e15f714c0ae888fac90a218c9ad
JefferDevop <77632174+JefferDevop@users.noreply.github.com>
2026-06-02 04:59:38 -0500

    Update BASE_API URL for the application

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const BASE_API =
-  "https://danitexdistribuidora.conexion.bar";
+  "https://danitexdistribuidora.cosmoqa.store";
 
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
